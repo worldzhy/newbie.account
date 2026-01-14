c6f65b563453aece661356d4cd61949923900869
wy-inceptionpad <48701263+wy-inceptionpad@users.noreply.github.com>
2026-01-14 22:11:07 +0800

    refactor: replace prisma middleware with extensions

--- modules/user/user.prisma.extension.ts
+++ modules/user/user.prisma.extension.ts
@@ -1,54 +1,60 @@
+import {Prisma} from '@generated/prisma/client';
 import {generateHash} from '@framework/utilities/common.util';
 import {verifyEmail, verifyPassword} from '@microservices/account/helpers/validator';
 import {BadRequestException} from '@nestjs/common';
-import {Prisma} from '@generated/prisma/client';
 
-export const prismaExtensionForUser = Prisma.defineExtension({
-  name: 'query-extension-usr',
-  query: {
-    user: {
-      async create({args, query}) {
-        if (args.data.email) {
-          if (!verifyEmail(args.data.email)) {
-            throw new BadRequestException('Your email is not valid.');
+export const userPrismaExtension = Prisma.defineExtension(prisma =>
+  prisma.$extends({
+    query: {
+      user: {
+        async $allOperations({model, operation, args, query}) {
+          if (operation === 'create' || operation === 'update') {
+            if (args.data) {
+              const {email, password, firstName, lastName, middleName, dateOfBirth} = args.data;
+
+              if (email && typeof email === 'string') {
+                if (!verifyEmail(email)) {
+                  throw new BadRequestException('Your email is not valid.');
+                }
+                args.data = {
+                  ...args.data,
+                  email: (email as string).toLowerCase().trim(),
+                };
+              }
+
+              if (password && typeof password === 'string') {
+                if (!verifyPassword(password)) {
+                  throw new BadRequestException(
+                    'The password is not strong enough. (length >= 8, lowercase >= 1, uppercase >= 1, numbers >= 1, symbols >= 1)'
+                  );
+                }
+                const hash = await generateHash(password);
+                args.data = {
+                  ...args.data,
+                  password: hash,
+                };
+              }
+
+              if (firstName && lastName && typeof firstName === 'string' && typeof lastName === 'string') {
+                const name =
+                  firstName + ' ' + (middleName && typeof middleName === 'string' ? middleName + ' ' : '') + lastName;
+                args.data = {
+                  ...args.data,
+                  name,
+                };
+              }
+
+              if (dateOfBirth) {
+                args.data = {
+                  ...args.data,
+                  dateOfBirth: new Date(dateOfBirth.toString()),
+                };
+              }
+            }
           }
-          args.data.email = args.data.email.toLowerCase();
-        }
-
-        if (args.data.password) {
-          if (!verifyPassword(args.data.password)) {
-            throw new BadRequestException(
-              'The password is not strong enough. (length >= 8, lowercase >= 1, uppercase >= 1, numbers >= 1, symbols >= 1)'
-            );
-          }
-          // Generate hash of the password.
-          const hash = await generateHash(args.data.password);
-          args.data.password = hash;
-        }
-
-        return query(args);
-      },
-      async update({args, query}) {
-        if (args.data.email) {
-          if (!verifyEmail(args.data.email as string)) {
-            throw new BadRequestException('Your email is not valid.');
-          }
-          args.data.email = (args.data.email as string).toLowerCase();
-        }
-
-        if (args.data.password) {
-          if (!verifyPassword(args.data.password as string)) {
-            throw new BadRequestException(
-              'The password is not strong enough. (length >= 8, lowercase >= 1, uppercase >= 1, numbers >= 1, symbols >= 1)'
-            );
-          }
-          // Generate hash of the password.
-          const hash = await generateHash(args.data.password as string);
-          args.data.password = hash;
-        }
-
-        return query(args);
+          return query(args);
+        },
       },
     },
-  },
-});
+  })
+);
